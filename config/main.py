5429498f758b949782633d6396f37666ab99b481
Giorgi Duchidze <giorgiduchidze2004@gmail.com>
2025-03-06 18:22:37 +0400

    Stage 2: Connect with MongoDB Atlas Embeddings Created and Uploaded, Working to develop semantic search functionality

--- config/main.py
+++ config/main.py
@@ -1,7 +1,12 @@
-import logging
+import logging, os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
 
 # logger.info("+")
+
+db_url = os.getenv('DATABASE_URL')
